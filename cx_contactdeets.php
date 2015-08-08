579eedde874e1ae89f803f81dbb2dc541a08e352
Pete Clark <pete@ablewild.com>
2015-08-08 17:42:39 +0100

    Add checkbox for including phone number in header and footer

--- cx_contactdeets.php
+++ cx_contactdeets.php
@@ -3,7 +3,7 @@
 Plugin Name: CX: Contact Details
 Plugin URI: http://www.ablewild.com
 Description: This plugin allows you to set contact information for inclusion in your website. This plugin is only licenced for the use of customers of Clark CX Ltd.
-Version: 1.2.1
+Version: 1.2.2
 Author: Pete Clark
 Author URI: http://www.ablewild.com/
 */
